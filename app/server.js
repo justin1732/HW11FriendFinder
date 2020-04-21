f485a86301d69c90de8e9401c0a03191cfdc74bf
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-04-20 19:28:41 -0500

    added routing

--- app/server.js
+++ app/server.js
@@ -14,6 +14,8 @@ app.use(bodyParser.raw({type: 'application/vnd.custom-type'}));
 
 app.use(bodyParser.text({type: 'text/html'}));
 
+require("../app/routing/htmlRoutes.js")(app);
+
 app.listen(PORT, function (){
     console.log("Friend Finding on PORT: " + PORT);
     });
