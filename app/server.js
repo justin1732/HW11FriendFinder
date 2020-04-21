cc06b000f4eb3cbce297bbba8d88117375547ead
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-04-21 01:27:16 -0500

    Cleaning up and final steps...

--- app/server.js
+++ app/server.js
@@ -4,9 +4,7 @@ var path = require ("path");
 var bodyParser = require ("body-parser");
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json();
-
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json({type: 'application/**json'}));
 
